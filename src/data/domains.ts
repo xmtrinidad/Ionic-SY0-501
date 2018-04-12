0c4b63d8635db651f5e140c09b988665c51c711e
Xavier Trinidad <xmtrinidad@gmail.com>
2018-04-11 17:29:47 -0700

    Updated app icon, changed some domain titles

--- src/data/domains.ts
+++ src/data/domains.ts
@@ -1,9 +1,9 @@
 import { Domain } from '../models/domain';
 
 export const DOMAINS: Domain[] = [
-  {id: 0, icon: 'fas fa-exclamation-triangle', title: '1.0 Threats/Vulnerabilities', color: '#512DA8' },
-  {id: 1, icon: 'fas fa-lock', title: '2.0 Technologies/Tools', color: '#0288D1' },
-  {id: 2, icon: 'fas fa-pencil-alt', title: '3.0 Architecture/Design', color: '#388E3C' },
+  {id: 0, icon: 'fas fa-exclamation-triangle', title: '1.0 Threats & Vulnerabilities', color: '#512DA8' },
+  {id: 1, icon: 'fas fa-lock', title: '2.0 Technologies & Tools', color: '#0288D1' },
+  {id: 2, icon: 'fas fa-pencil-alt', title: '3.0 Architecture & Design', color: '#388E3C' },
   {id: 3, icon: 'fas fa-id-badge', title: '4.0 Access Management', color: '#283593' },
   {id: 4, icon: 'fas fa-clipboard-list', title: '5.0 Risk Management', color: '#C2185B' },
   {id: 5, icon: 'fas fa-user-secret', title: '6.0 Cryptography', color: '#00897B' }
